b207c1a3e3fe091f51a6861bc078a4d08763fea6
jlsanchezdelgado95 <kokekui17@gmail.com>
2019-04-07 19:36:47 +0200

    Empezado por la tarde

--- js.js
+++ js.js
@@ -2,43 +2,27 @@ window.onload = function () {
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/categorias",
-        //data: "data",
-        //dataType: "dataType",
         success: function (json) {
             $("#categorias").html('');
-            for (let index = 0; index < json.length; index++) {//PROBAR CON MAP Y CON EACH, mas sencillo(this.nombre...)
-                $("#categorias").append("<a href='#'>" + json[index]["nombre"] + "</a>");
-                //console.log(json[index]["id"]);
-                //console.log(json[index]);
-                //console.log(json[index]["nombre"]);
-            }
-            //console.log(json);
-            //console.log(json.nombre);
+            json.map(elemento => {
+                $("#categorias").append("<a href='#' id=" + elemento.id + " onclick=mostrarCategoria()>" + elemento.nombre + "</a>");
+            });
         }
     });
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/fotos",
-        //data: "data",
-        //dataType: "dataType",
         success: function (json) {
-            for (let index = 0; index < json.length; index++) {
-                console.log(json[index]["activa"]);
-                if (json[index]["activa"] === "true") {
-                    $(".carousel-inner").append("<div class='carousel-item active'><img src=" + json[index]["link"] + " class='d-block w-100' alt='...'></img></div>");
-                    console.log("HOLA");
+            json.map(elemento => {
+                if (elemento.activa === "true") {
+                    $(".carousel-inner").append("<div class='carousel-item active'><img src=" + elemento.link + " class='d-block w-100' alt='...'></img></div>");
                 } else {
-                    $(".carousel-inner").append("<div class='carousel-item'><img src=" + json[index]["link"] + " class='d-block w-100' alt='...'></img></div>");
+                    $(".carousel-inner").append("<div class='carousel-item'><img src=" + elemento.link + " class='d-block w-100' alt='...'></img></div>");
                 }
-            }
+            });
         }
     });
-    /*$.get({
-        url: "tienda.json",
-        data: "data",
-        dataType: "JSON",
-        success: function (json) {
-            $(json.tittle).appendTo("categorias");
-        }
-    });*/
+    function mostrarCategoria() {
+        $("#articulos").html('');
+    }
 }
