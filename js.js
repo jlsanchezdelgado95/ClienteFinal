22c555aac90623e48372d808e52d162cae793c5a
kokekui <kokekui17@gmail.com>
2019-04-15 00:05:26 +0200

    Corregir modal y crear objetos

--- js.js
+++ js.js
@@ -6,31 +6,55 @@ window.onload = function () {
             $("#categorias").html('');
             json.map(elemento => {//Mejor hacerlo con $categorias.on(click,clase,function...)
                 console.log(elemento.id);
-                idCat=elemento.id;
+                //idCat=elemento.id; No hace nada relevante(ESTOY PINTANDO CATEGORIAS)
                 $("#categorias").append("<a href='#' id=" + elemento.id + ">" + elemento.nombre + "</a>");
+                if (elemento.activa == "true") {
+                    $.ajax({
+                        type: "GET",
+                        url: "http://localhost:3000/articulos?idCat=" + elemento.id + "",
+                        success: function (json) {
+                            json.map(elemento => {
+                                console.log(elemento.id);
+                                $("#articulos").append("<div href='#' id=art" + elemento.id + ">" + elemento.nombre + "<br>" + elemento.descripcion + "<br>" + elemento.precio + 
+                                "<button class='btn btn-info'>Añadir al Carrito</button></div>");
+                            });
+                        }
+                    });
+                }
             });
         }
     });
 
-    $("#categorias").on("click","a",function(){
+    $("#categorias").on("click","a",function(){//FUNCION PARA PINTAR PRODUCTOS DE UNA CATEGORIA
         $("#articulos").html('');
         idCat=($(this).attr("id"));
         $.ajax({
             type: "GET",
-            url: "http://localhost:3000/articulos?idCat=" + idCat+ "",
+            url: "http://localhost:3000/articulos?idCat=" + idCat + "",
             success: function (json) {
                 json.map(elemento => {
-                    console.log("HOLA");
-                    $("#articulos").append("<div href='#' id=art" + elemento.id + " onclick=mostrarCategoria()>" + elemento.nombre + "</div>");
+                    console.log(elemento.id);
+                    $("#articulos").append("<div href='#' id=art" + elemento.id + ">" + elemento.nombre + "<br>" + elemento.descripcion + "<br>" + elemento.precio + 
+                    "<button class='btn btn-info'>Añadir al Carrito</button></div>");
                 });
             }
         });
     })
-    
-    
-    
-    
-    
+
+    $("#articulos").on("click","button",function(){
+        idArt = $("div:nth-child(2)");
+        console.log(idArt);
+        $.ajax({
+            type: "method",
+            url: "http://localhost:3000/articulos?id=" + idArt + "",
+            success: function (elemento) {
+                
+            }
+        });
+        $("#myModal").modal("show");
+
+    })
+
     //Original JL con onclick
    /* 
     $.ajax({
@@ -46,7 +70,7 @@ window.onload = function () {
         }
     });
     */
-    $.ajax({
+    $.ajax({//CARRUSEL FOTOS
         type: "GET",
         url: "http://localhost:3000/fotos",
         success: function (json) {
@@ -60,12 +84,12 @@ window.onload = function () {
         }
     });
 }
-function mostrarCategoria(idCat) {
+function mostrarProducto(idProd) {
     $("#articulos").html('');
-    console.log(idCat);
+    console.log(idProd);
     $.ajax({
         type: "GET",
-        url: "http://localhost:3000/articulos?idCat=" + idCat+ "",
+        url: "http://localhost:3000/articulos?id=" + idProd+ "",
         success: function (json) {
             json.map(elemento => {
                 console.log("HOLA");
