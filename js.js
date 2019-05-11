bef6192e91a1a5c2a88790c461a3cafb86ea3cb7
kokekui <kokekui17@gmail.com>
2019-05-11 02:14:33 +0200

    Se añaden carritos a la base de datos, limpiar codigo, mirar por futuros carritos y meter el crud

--- js.js
+++ js.js
@@ -1,117 +1,122 @@
 window.onload = function () {
-    //micarrito=new carrito()
-    function Carrito(fecha, numero) {
+    micarrito = new Carrito("11/05/2019", 1);
+    $("#bienvenida").modal("show");
+
+        //OBJETO ARTICULO
+        function Articulo(codigo, descripcion, precio) {
+            this.codigo = codigo;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.unidades = 1;
+            //al crearse, minimo es uno
+        }
+        //FIN DE OBJETO ARTICULO
+    ////// OBJETO CARRITO
+    function Carrito(fecha, id) {
         this.fecha = fecha;
-        this.numero = numero;
+        this.id = id;
         this.articulos = [];
     }
 
-    Carrito.prototype.anyadirArticulo = function(articuloNuevo) {
+    Carrito.prototype.anyadirArticulo = function (articuloNuevo) {
         let yaExiste = false;
-      //Compruebo si existe el nuevo articulo, en el array de articulos
-      // Y si existe, añade unidades, si no, se hace un push al array articulos
         this.articulos.forEach(articulo => {
-          if (articulo.codigo === articuloNuevo.codigo) {
-            articulo.unidades++;
-            yaExiste = true;
-          }
+            if (articulo.codigo === articuloNuevo.codigo) {
+                articulo.unidades++;
+                yaExiste = true;
+            }
         });
-        // no añadir al array si ya estaba en el carrito y se ha sumado una unidad arriba
+        // Si no esta en el array de articulos, añadir
         if (!yaExiste) {
-          this.articulos.push(articuloNuevo);
+            this.articulos.push(articuloNuevo);
         }
-      }
-      
+    }
+    // FIN DE OBJETO CARRITO
     $.ajax({
         type: "GET",
         url: "http://localhost:3000/categorias",
         success: function (json) {
             $("#categorias").html('');
             json.map(elemento => {
-                console.log(elemento.id);
                 $("#categorias").append("<a href='#' id=" + elemento.id + ">" + elemento.nombre + "</a>");
                 if (elemento.activa == "true") {
-                    $.ajax({
-                        type: "GET",
-                        url: "http://localhost:3000/articulos?idCat=" + elemento.id + "",
-                        success: function (json) {
-                            json.map(elemento => {
-                                console.log(elemento.id);
-                                $("#articulos").append("<div href='#' id=art" + elemento.id + ">" + elemento.nombre + "<br>" + elemento.descripcion + "<br>" + elemento.precio +
-                                    "<button class='btn btn-info'>Añadir al Carrito</button></div>");
-                            });
-                        }
-                    });
+                    pintarArt(elemento.id);
                 }
             });
         }
     });
 
-    $("#categorias").on("click", "a", function () {
-        $("#articulos").html('');
-        idCat = ($(this).attr("id"));
-        console.log(idCat);
+    function pintarArt(id) {
         $.ajax({
             type: "GET",
-            url: "http://localhost:3000/articulos?idCat=" + idCat + "",
+            url: "http://localhost:3000/articulos?idCat=" + id + "",
             success: function (json) {
                 json.map(elemento => {
-                    console.log(elemento.id);
                     $("#articulos").append("<div href='#' id=art" + elemento.id + "><p>" + elemento.nombre + "</p><p>" + elemento.descripcion + "</p><p>" + elemento.precio +
                         "</p><button class='btn btn-info'>Añadir al Carrito</button></div>");
                 });
             }
         });
+    }
+
+    $("#categorias").on("click", "a", function () {
+        $("#articulos").html('');
+        idCat = ($(this).attr("id"));
+        pintarArt(idCat);
     })
 
     $("#articulos").on("click", "button", function () {
-        cod = $(this).parent().attr("id")
-        console.log(cod)
-        nombre = $(this).parent().find("p:nth-child(1)").html()
-        descripcion = $(this).parent().find("p:nth-child(2)").html()
-        precio = $(this).parent().find("p:nth-child(3)").html()
-        console.log(precio);
-        $("#cuerpo").html("");
-        $("#cuerpo").append("<h3>" + cod + "</h3>");
-        $("#cuerpo").append("<p>" + nombre + "</p>");
-        $("#cuerpo").append("<p>" + descripcion + "</p>");
-        $("#cuerpo").append("<p>" + precio + "</p>");
+        codArt = $(this).parent().attr("id");
+        nomArt = $(this).parent().find("p:eq(0)").html();
+        desArt = $(this).parent().find("p:eq(1)").html();
+        pvpArt = $(this).parent().find("p:eq(2)").html();
+        $("#cuerpoArt").html("");
+        $("#cuerpoArt").append("<h3>" + codArt + "</h3>");
+        $("#cuerpoArt").append("<p>" + nomArt + "</p>");
+        $("#cuerpoArt").append("<p>" + desArt + "</p>");
+        $("#cuerpoArt").append("<p>" + pvpArt + "</p>");
         $("#myModal").modal("show");
-
     });
 
-    $("#btn-comprar").click(function () {//COMPROBAR CON EL NTH-CHILD
-        console.log("Comprando...")
-        codArt = $("#cuerpo").find("h3:eq(0)").html();
-        nomArt = $("#cuerpo").find("p:eq(0)").html();
-        desArt = $("#cuerpo").find("p:eq(1)").html();
-        pvpArt = $("#cuerpo").find("p:eq(2)").html();
-        console.log(codArt);
-        console.log(nomArt);
-        console.log(desArt);
-        console.log(pvpArt);
+    $("#carrito").click(function () {//Funcion para pintar todo el carro
+        $("#articulos").html('');
+        $("#articulos").append("<p>Su carrito tiene fecha de: " + micarrito.fecha + "</p>");
+        $("#articulos").append("<p>Su numero de carrito es: " + micarrito.numero + "</p>");
+        $("#articulos").append("<p>Sus articulos son : </p>");
+        for (let index = 0; index < micarrito.articulos.length; index++) {
+            $("#articulos").append("<p>Codigo Articulo: " + micarrito.articulos[index].codigo + "</p>");
+            $("#articulos").append("<p>Descripcion Articulo: " + micarrito.articulos[index].descripcion + "</p>");
+            $("#articulos").append("<p>Precio Articulo: " + micarrito.articulos[index].precio + "</p>");
+            $("#articulos").append("<p>Unidades Articulo: " + micarrito.articulos[index].unidades + "</p>");
+        }
+        $("#articulos").append("<div id=compraJSON class='btn btn-info'>Comprar</div>");
+    });
 
-        //miarticulo=new Articulo(codArt,nomArt,desArt,pvpArt)
+    $("#compraJSON").click(function () {//funcion para meter en el json
+        console.log("HOLA");
+        datos = JSON.stringify(micarrito);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/carritos",
+            data: datos,
+            contentType: 'application/json',
+            success: function (response) {
+                console.log("FUNCIONA");
+            }
+        });
+    });
 
-        //micarrito.anyadir(miarticulo)
+    $("#btn-comprar").click(function () {
+        codArt = $("#cuerpoArt").find("h3:eq(0)").html();
+        nomArt = $("#cuerpoArt").find("p:eq(0)").html();
+        desArt = $("#cuerpoArt").find("p:eq(1)").html();
+        pvpArt = $("#cuerpoArt").find("p:eq(2)").html();
 
+        miarticulo = new Articulo(codArt, nomArt, desArt, pvpArt)
+        micarrito.anyadirArticulo(miarticulo);
+        //$("#myModal").modal("hide");//OPCIONAL
     })
 
-    //Original JL con onclick
-    /* 
-     $.ajax({
-         type: "GET",
-         url: "http://localhost:3000/categorias",
-         success: function (json) {
-             $("#categorias").html('');
-             json.map(elemento => {//Mejor hacerlo con $categorias.on(click,clase,function...)
-                 console.log(elemento.id);
-                 idCat=elemento.id;
-                 $("#categorias").append("<a href='#' id=" + elemento.id + " onclick=mostrarCategoria('"+idCat+"')>" + elemento.nombre + "</a>");
-             });
-         }
-     });
-     */
     $.ajax({//CARRUSEL FOTOS
         type: "GET",
         url: "http://localhost:3000/fotos",
@@ -125,18 +130,18 @@ window.onload = function () {
             });
         }
     });
-}
-function mostrarProducto(idProd) {
-    $("#articulos").html('');
-    console.log(idProd);
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/articulos?id=" + idProd + "",
-        success: function (json) {
-            json.map(elemento => {
-                console.log("HOLA");
-                $("#articulos").append("<div href='#' id=art" + elemento.id + " onclick=mostrarCategoria()>" + elemento.nombre + "</div>");
-            });
-        }
-    });
+
+    /*function mostrarProducto(idProd) {
+        $("#articulos").html('');
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/articulos?id=" + idProd + "",
+            success: function (json) {
+                json.map(elemento => {
+                    console.log("HOLA");
+                    $("#articulos").append("<div href='#' id=art" + elemento.id + " onclick=mostrarCategoria()>" + elemento.nombre + "</div>");
+                });
+            }
+        });
+    }*/
 }
